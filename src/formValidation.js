7f350689d5137e3fe3dfbf579f0776866d7fada2
elishamutang <elishamutang@outlook.com>
2024-04-29 23:28:59 +0800

    initial stage.

---
+++ src/formValidation.js
@@ -0,0 +1,6 @@
+import buildingPic from '/assets/danist-soh-dqXiw7nCb9Q-unsplash.jpg'
+
+export default function generateForm() {
+    const imageTag = document.querySelector('img')
+    imageTag.src = buildingPic
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css'
+import generateForm from './formValidation'
+
+generateForm()
