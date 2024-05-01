fce0829ee4efe6a0f49e062d50859df23ffe6c91
elishamutang <elishamutang@outlook.com>
2024-05-01 20:56:45 +0800

    [refactor] Extended formValidation.js

--- src/formValidation.js
+++ src/formValidation.js
@@ -1,6 +1,38 @@
-import buildingPic from '/assets/danist-soh-dqXiw7nCb9Q-unsplash.jpg'
+export default function formValidation() {
+    const formElem = document.querySelector('form')
 
-export default function generateForm() {
-    const imageTag = document.querySelector('img')
-    imageTag.src = buildingPic
+    // User object
+    const userDetails = {
+        uemail: '',
+        pwd: '',
+        pwdConfirm: '',
+        country: '',
+        zipCode: '',
+    }
+
+    // When user is typing.
+    formElem.addEventListener('input', (e) => {
+        if (e.target.tagName === 'INPUT') {
+            if (!e.target.checkValidity()) {
+                showError(e.target)
+            }
+        }
+    })
+
+    // Saves value when user clicks out of input field.
+    formElem.addEventListener('focusout', (e) => {
+        if (e.target.tagName === 'INPUT') {
+            Object.keys(userDetails).forEach((key) => {
+                if (key === e.target.id) {
+                    console.log(e.target.checkValidity())
+                    userDetails[key] = e.target.value
+                }
+            })
+        }
+        console.log(userDetails)
+    })
+}
+
+function showError(inputValue) {
+    console.log(inputValue)
 }
