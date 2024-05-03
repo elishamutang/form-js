0837163ed00c032137d5f6752c487810df13ee86
elishamutang <elishamutang@outlook.com>
2024-05-03 21:51:07 +0800

    [build] new build.

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,80 @@
-(()=>{"use strict";var t={};t.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),(()=>{var r;t.g.importScripts&&(r=t.g.location+"");var e=t.g.document;if(!r&&e&&(e.currentScript&&(r=e.currentScript.src),!r)){var i=e.getElementsByTagName("script");if(i.length)for(var c=i.length-1;c>-1&&(!r||!/^http(s?):/.test(r));)r=i[c--].src}if(!r)throw new Error("Automatic publicPath is not supported in this browser");r=r.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),t.p=r})();const r=t.p+"danist-soh-dqXiw7nCb9Q-unsplash.jpg";document.querySelector("img").src=r})();
+;(() => {
+    'use strict'
+    var t = {}
+    ;(t.g = (function () {
+        if ('object' == typeof globalThis) return globalThis
+        try {
+            return this || new Function('return this')()
+        } catch (t) {
+            if ('object' == typeof window) return window
+        }
+    })()),
+        (() => {
+            var e
+            t.g.importScripts && (e = t.g.location + '')
+            var r = t.g.document
+            if (!e && r && (r.currentScript && (e = r.currentScript.src), !e)) {
+                var c = r.getElementsByTagName('script')
+                if (c.length)
+                    for (
+                        var n = c.length - 1;
+                        n > -1 && (!e || !/^http(s?):/.test(e));
+
+                    )
+                        e = c[n--].src
+            }
+            if (!e)
+                throw new Error(
+                    'Automatic publicPath is not supported in this browser'
+                )
+            ;(e = e
+                .replace(/#.*$/, '')
+                .replace(/\?.*$/, '')
+                .replace(/\/[^\/]+$/, '/')),
+                (t.p = e)
+        })()
+    const e = t.p + 'danist-soh-dqXiw7nCb9Q-unsplash.jpg'
+    ;(document.querySelector('img').src = e),
+        (function () {
+            const t = document.querySelector('form'),
+                e = {
+                    uemail: '',
+                    pwd: '',
+                    pwdConfirm: '',
+                    country: '',
+                    zipCode: '',
+                }
+            t.addEventListener('input', (t) => {
+                var e
+                'INPUT' === t.target.tagName &&
+                    (t.target.checkValidity() ||
+                        ((e = t.target), console.log(e.parentElement)))
+            }),
+                t.addEventListener('focusout', (t) => {
+                    'INPUT' === t.target.tagName &&
+                        Object.keys(e).forEach((r) => {
+                            r === t.target.id && (e[r] = t.target.value)
+                        }),
+                        '' !== e.zipCode &&
+                            (async function (t) {
+                                const e = new URL(
+                                    '"https://api.zipcodestack.com/v1/search"'
+                                )
+                                e.searchParams.append('codes', t),
+                                    fetch(e, {
+                                        method: 'GET',
+                                        headers: {
+                                            apikey: '01HWT573XA2A9RBTX16QRRJ9N8',
+                                            Accept: 'application/json',
+                                        },
+                                    })
+                                        .then((t) => t.json())
+                                        .catch((t) => t)
+                                        .then((t) => t.results)
+                                        .then((t) => {
+                                            console.log(t)
+                                        })
+                            })(e.zipCode)
+                })
+        })()
+})()
