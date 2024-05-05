51571b46b3b4e6a5b523a430c0d7ab94728784a2
elishamutang <elishamutang@outlook.com>
2024-05-05 15:30:32 +0800

    [refactor] Added async function, fetch data from external APIs to validate country and zipcode. Added more to constraint validation.

--- src/formValidation.js
+++ src/formValidation.js
@@ -16,7 +16,17 @@ export default function formValidation() {
     formElem.addEventListener('input', (e) => {
         if (e.target.tagName === 'INPUT') {
             if (!e.target.checkValidity()) {
+                console.log(e.target.value)
+
+                e.target.parentElement.querySelector('span').className +=
+                    ' active'
+
                 showError(e.target)
+            } else {
+                e.target.parentElement.querySelector('span').className =
+                    'errorMsg'
+
+                e.target.parentElement.querySelector('span').textContent = ''
             }
         }
     })
@@ -31,44 +41,95 @@ export default function formValidation() {
             })
         }
         // Ensure zip code entered by user is valid.
-        if (userDetails.zipCode !== '') {
-            validateZipCode(userDetails.zipCode)
+        if (userDetails.zipCode !== '' && userDetails.country !== '') {
+            validateZipCode(userDetails.zipCode, userDetails.country)
+                .then((result) => {
+                    console.log(result)
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         }
     })
 }
 
 function showError(inputElem) {
-    console.log(inputElem.parentElement)
+    const divElem = inputElem.parentElement
+    const spanElem = divElem.querySelector('span')
+
+    if (inputElem.validity.typeMismatch) {
+        spanElem.textContent = 'Invalid email address'
+    }
+
+    if (inputElem.validity.valueMissing) {
+        spanElem.textContent = 'Required field'
+    }
+
+    if (inputElem.validity.patternMismatch) {
+        spanElem.textContent = 'Invalid input'
+    }
 }
 
-async function validateZipCode(userZipCode) {
+async function validateZipCode(userZipCode, userCountry) {
+    // ZipCodeStack API
     const zipCodeStackURL = new URL('https://api.zipcodestack.com/v1/search')
 
     zipCodeStackURL.searchParams.append('codes', userZipCode)
     zipCodeStackURL.searchParams.append('apikey', myKey)
 
-    console.log(zipCodeStackURL)
+    console.log(userCountry)
 
+    // REST Countries API.
+    const countriesApi = `https://restcountries.com/v3.1/name/${userCountry}`
+
+    // Fetch zip code data
     const zipCodeResults = fetch(zipCodeStackURL, {
         method: 'GET',
     })
         .then((response) => {
             return response.json()
         })
-        .catch((err) => {
-            return err
-        })
-
-    zipCodeResults
         .then((response) => {
             return response.results
         })
-        .then((results) => {
-            console.log(results)
+        .catch((err) => {
+            console.error(err)
         })
 
-    // We want to check whether country has the correct zip code. To do this:
-    // 1. Get country input from input field.
-    // 2. Lookup country code
-    // 3. Check country code exists in the results for the input zip code.
+    // Fetch country information
+    const countryResults = fetch(countriesApi, {
+        method: 'GET',
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            return result
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+
+    // Wait till all promises are settled.
+    return Promise.all([countryResults, zipCodeResults])
+        .then(([country, zipCode]) => {
+            const [countryCode] = country[0].altSpellings
+            console.log(countryCode)
+
+            const zipCodeInput = zipCode[userZipCode]
+
+            // Initial result
+            let result = false
+
+            zipCodeInput.forEach((obj) => {
+                if (obj.country_code === countryCode) {
+                    result = true
+                }
+            })
+
+            return result
+        })
+        .then((result) => {
+            return result
+        })
 }
