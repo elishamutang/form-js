3a3a52007b069f57c15cb43c3d5257fb18331bc4
elishamutang <elishamutang@outlook.com>
2024-05-06 23:18:12 +0800

    [refactor] Replaced cover img.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css'
 import formValidation from './formValidation'
-import buildingPic from '/assets/danist-soh-dqXiw7nCb9Q-unsplash.jpg'
+import buildingPic from '/assets/coverPic.jpg'
 
 // Append cover image
 const imageTag = document.querySelector('img')
